0d1cf09d74f820b89a029e2a67d4eefaab8666d5
tima20 <tima20@vt.edu>
2024-03-26 19:44:25 -0400

    Rewrote read_url method to return a dict of all the cards at the url, and the total amount of cards retrieved

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import json
 import requests
 
-def read_page(json_data, card_count):
-    # Print the response data (usually JSON)
-    for i in range(0, len(json_data['data'])):
-        card_count += 1
-        card = json_data['data'][i]
-        print("Card", card_count, "out of", json_data['total_cards'], card['name'])
-    
-    return card_count
 
-def read_url(url, card_count):
+def read_url(url, json_data):
     response = requests.get(url)
+
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
-        # Print the response data (usually JSON)
-        json_data = response.json()
-        card_count = read_page(json_data, card_count)
+        page = response.json()
+        if (json_data is {}):
+            json_data.update(page['total_cards'])
+        
+        # Read the page and add it to json_data
+        json_data.update(page)
 
-        if (json_data['has_more']):
-            read_url(json_data['next_page'], card_count)
+        # If there is another page, call read_url again
+        if json_data['has_more']:
+            json_data = read_url(json_data['next_page'], json_data)
+        
+        return json_data
 
     else:
         # Print an error message if the request was not successful
         print('Error:', response.status_code)
 
 url = 'https://api.scryfall.com/cards/search?as=grid&order=name&q=%28game%3Apaper%29+set%3Amkm'
-read_url(url, 0)
+data = read_url(url, {})
+print(data['total_cards'])
