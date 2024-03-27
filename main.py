ebf385852a31697279f77e472310db7e79b2ab0c
tima20 <tima20@vt.edu>
2024-03-26 20:13:57 -0400

    Rewrote read_url again. Now uses recursive helper method read_page to generate a list of cards in card_data, then passes it back as a tuple containing the total amount of cards then card_data

--- main.py
+++ main.py
@@ -1,29 +1,49 @@
 import json
 import requests
 
-
-def read_url(url, json_data):
+# card_data: array of dicts, with each dict being a card
+# page_data: dict with several elements, including an array of cards (card_data)
+def read_url(url):
     response = requests.get(url)
 
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
-        page = response.json()
-        if (json_data is {}):
-            json_data.update(page['total_cards'])
+        page_data = response.json()
+        card_data = read_page(url, [])
         
-        # Read the page and add it to json_data
-        json_data.update(page)
-
-        # If there is another page, call read_url again
-        if json_data['has_more']:
-            json_data = read_url(json_data['next_page'], json_data)
-        
-        return json_data
+        return page_data['total_cards'], card_data
 
     else:
         # Print an error message if the request was not successful
         print('Error:', response.status_code)
 
+def read_page(url, card_data):
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        page_data = response.json()
+
+        # Read the page and add it to json_data
+        card_data.extend(page_data['data'])
+
+        # If there is another page, call read_url again
+        if page_data['has_more']:
+            card_data = read_page(page_data['next_page'], card_data)
+        
+        return card_data
+
+    else:
+        # Print an error message if the request was not successful
+        print('Error:', response.status_code)
+
+commons = {}
+uncommons = {}
+rares = {}
+mythics = {}
+
 url = 'https://api.scryfall.com/cards/search?as=grid&order=name&q=%28game%3Apaper%29+set%3Amkm'
-data = read_url(url, {})
-print(data['total_cards'])
+num_cards, card_list = read_url(url)
+
+for card in card_list:
+    print(card['name'])
