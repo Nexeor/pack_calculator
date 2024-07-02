8dfeeb5d2b8c72c3de94999f8efe94f6cfada75d
tima20 <tima20@vt.edu>
2024-07-02 15:56:46 -0400

    Only two files needed now. Categoriers are read from mh3_categories and written as JSON to mh3_cards.

--- main.py
+++ main.py
@@ -29,33 +29,31 @@ def read_page(url):
         return None
     
 # Given a list of URLs, read the cards from them and organize it in a JSON file
-def organize_categories(input, output, categories):
-    input_file = open(input, 'r')
-    output_file = open(output, 'w')
-    out = {}
+def organize_categories(input_file, output_file):
+    input = open(input_file, 'r')
+    output = open(output_file, 'w')
+    set = {}
     
-    for line in input_file:
-        # Parse the category/URL and gather the card list
-        parsed = line.split(';')
-        category_name = parsed[0]
-        print("Reading from URL: " + parsed[1])
-        cards = read_url(parsed[1])
+    for line in input:
+        # Parse the category/URL to gather the card list
+        print(line.split(';'))
+        name, url = line.split(';')
+        print("Reading from URL: " + url)
+        url_cards_full = read_url(url)
         
-        # Create a dict out of the gathered cards
-        category = []
-        for i in range(0, len(cards)):
-            category.append(cards[i]['name'])
+        # Create a seperate list with only the needed information
+        url_cards_simple = []
+        for i in range(0, len(url_cards_full)):
+            url_cards_simple.append(url_cards_full[i]['name'])
         
-        full_category = { 'num_cards' : len(cards), 'cards' : category }
-        print(full_category)
-        out[category_name] = full_category
+        # Format the list correctly and add it to the larger set
+        full_category = { 'num_cards' : len(url_cards_full), 'cards' : url_cards_simple }
+        set[name] = full_category
 
-    # Write the dict as a JSON file 
-    # output_file.write(json.dumps({categories}, indent=4))
-    print(json.dumps(out, indent = 4))
+    # Write the set to a JSON file
+    output.write(json.dumps(set, indent = 4))
 
 
 category_file = 'mh3_categories.txt'
-card_file = 'mh3_cards.txt'
-categories = {}
-organize_categories(category_file, card_file, categories)
+card_file = 'mh3_cards.json'
+organize_categories(category_file, card_file)
